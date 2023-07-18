89e7d0c55ecdb4ed576ea87eced551ecb70865ad
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-07-18 18:34:10 +0200

    Search section styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,19 @@
-import "./SearchBar.css"
+import "./SearchBar.css";
 
-export function SearchBar () {
-
+export function SearchBar() {
   return (
     <div className="search-section">
-      <h1><b>How can we help?</b></h1>
+
+      <h1>
+        <b>How can we help?</b>
+      </h1>
+
       <div className="searchbar">
-        <a></a>
-        <input type="text" placeholder="Search"></input>
+        <form role="search" method="get" action="">
+          <input type="search" id="query" placeholder="Search"></input>
+        </form>
       </div>
+      
     </div>
-  )
+  );
 }
