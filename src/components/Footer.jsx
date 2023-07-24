91c6242e3ef85179718a7dc6b16ab291f33e307e
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-07-24 11:57:36 +0200

    Footer minors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
-import "./Footer.css"
-import logo from "../assets/img/abstract-logo.png"
+import "./Footer.css";
+import logo from "../assets/img/abstract-logo.png";
 
-export function Footer () {
-
+export function Footer() {
   return (
     <div className="footer-container">
       <div className="abstract-links">
@@ -13,36 +12,40 @@ export function Footer () {
       </div>
 
       <div className="resources-links">
-      <h6>Resources</h6>
-      <a href="">Blog</a>
-      <a href="">Help Center</a>
-      <a href="">Release Notes</a>
-      <a href="">Status</a>
+        <h6>Resources</h6>
+        <a href="">Blog</a>
+        <a href="">Help Center</a>
+        <a href="">Release Notes</a>
+        <a href="">Status</a>
       </div>
 
       <div className="community-links">
-      <h6>Community</h6>
-      <a href="">Twitter</a>
-      <a href="">LinkedIn</a>
-      <a href="">Facebook</a>
-      <a href="">Dribbble</a>
-      <a href="">Podcast</a>
+        <h6>Community</h6>
+        <a href="">Twitter</a>
+        <a href="">LinkedIn</a>
+        <a href="">Facebook</a>
+        <a href="">Dribbble</a>
+        <a href="">Podcast</a>
       </div>
 
       <div className="company-links">
-      <h6>Company</h6>
-      <a href="">About Us</a>
-      <a href="">Careers</a>
-      <a href="">Legal</a>
-      <br />
-      <p>Contact Us</p>
-      <a href="mailto: mail@mail.com">info@abstract.com</a>
+        <h6>Company</h6>
+        <a href="">About Us</a>
+        <a href="">Careers</a>
+        <a href="">Legal</a>
+        <br />
+        <p>Contact Us</p>
+        <a href="mailto: mail@mail.com">info@abstract.com</a>
       </div>
 
       <div className="copyright-info">
-      <img src={logo} style={{ maxWidth: "25px"}} alt="Corp Logo"></img>
+        <img src={logo} style={{ maxWidth: "25px" }} alt="Corp Logo"></img>
+        <p>
+          © Copyright 2023<br />
+          Abstract Studio Design, Inc.<br />
+          All rights reserved<br />
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
